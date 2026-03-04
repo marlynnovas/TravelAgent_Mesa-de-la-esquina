a6b6ee19085a6cfd6321b4873b2df86318820626
Marlyn Novas <marlynysabelnovas@gmail.com>
2026-03-04 13:24:58 -0400

    messages

---
+++ Travelagent.py
@@ -0,0 +1,68 @@
+import requests
+
+BASE_URL = "https://restcountries.com/v3.1/all?fields=name,capital,region,subregion,population,currencies,languages,flags,timezones"
+
+def get_country_kpi():
+    country_name = input("Enter country name: ").strip().lower()
+    response = requests.get(BASE_URL)
+    countries = response.json()
+
+    #Siempre era un show con lo mismo que daba error porque o se escribia con mayuscula o no. 
+    country = next(
+        (c for c in countries if country_name in (c.get("name", {}).get("official", "").lower(),
+                                                  c.get("name", {}).get("common", "").lower())),
+        None
+    )
+
+    if not country:
+        print("Country not found.")
+        return
+
+    # Extract main fields
+    name = country["name"]["official"]
+    capital = country.get("capital", ["N/A"])[0]
+    region = country.get("region", "N/A")
+    subregion = country.get("subregion", "N/A")
+    population = country.get("population", 0)
+    timezones = ", ".join(country.get("timezones", []))
+    flag = country.get("flags", {}).get("png", "N/A")
+
+    # First currency
+    currencies = country.get("currencies", {})
+    if currencies:
+        first_currency = list(currencies.values())[0].get("name", "N/A")
+    else:
+        first_currency = "N/A"
+
+    # Languages
+    languages = country.get("languages", {})
+    if languages:
+        lang_list = [v if isinstance(v, str) else v.get("name","N/A") for v in languages.values()]
+        languages_str = ", ".join(lang_list)
+    else:
+        languages_str = "N/A"
+
+    # Print KPIs
+    print("\n===== COUNTRY Information =====")
+    print(f"Name: {name}")
+    print(f"Capital: {capital}")
+    print(f"Region / Subregion: {region} / {subregion}")
+    print(f"Population: {population:,}")
+    print(f"Currency: {first_currency}")
+    print(f"Languages: {languages_str}")
+    print(f"Time Zones: {timezones}")
+    print(f"Flag: {flag}")
+    print("=======================\n")
+
+def main():
+    while True:
+        choice = input("1. Country search & Information \n2. Trip Planning and  Costo estimations \nChoose: ").strip()
+        if choice == "1":
+            get_country_kpi()
+        elif choice == "2":
+            break
+        else:
+            print("Invalid option.")
+
+if __name__ == "__main__":
+    main()
---
+++ flet.py
@@ -0,0 +1,11 @@
+import flet as ft
+import requests
+from Travelagent import *
+
+def main(page: ft.Page):
+    page.title= "CARICOM Travel Agency"
+    page.theme_mode= ft.ThemeMode.LIGHT
+    page.padding= 20
+    page.window_width= 1000
+    page.window_height= 700
+
