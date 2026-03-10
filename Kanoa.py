241c2f5d797f87bde9bc71d195f8b538428353d4
emirethsoto <emishddez30@gmail.com>
2026-03-10 11:17:20 -0400

    Definir get country lo del name del pais y se pone eh el nombre

--- Kanoa.py
+++ Kanoa.py
@@ -1,3 +1,16 @@
 import flet as ft
 import requests
-#uhuuu
+
+BASE_URL = "https://restcountries.com/v3.1/all?fields=name,capital,region,subregion,population,currencies,languages,flags,timezones"
+
+def get_country_kpi():
+    country_name = input("Enter country name: ").strip().lower()
+    response = requests.get(BASE_URL)
+    countries = response.json()
+
+    #Siempre era un show con lo mismo que daba error porque o se escribia con mayuscula o no. 
+    country = next(
+        (c for c in countries if country_name in (c.get("name", {}).get("official", "").lower(),
+                                                  c.get("name", {}).get("common", "").lower())),
+        None
+    )
